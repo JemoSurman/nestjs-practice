c7b3af561f659a25fa7ef53e538f9f3e3bd49d39
JemoSurman <jemosurman@gmail.com>
2026-03-09 22:39:52 +0400

    DI & the controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
-
+import { UsersService } from './users.service';
 @Controller('users')
 export class UsersController {
-    
+
+    constructor(private readonly userService: UsersService){}
+
     @Get() // GET /users
     findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN'){
         return []
