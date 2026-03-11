f5506662708a4e2f2c81449cca2570b2642268a6
JemoSurman <jemosurman@gmail.com>
2026-03-11 23:50:50 +0400

    Applying ValidationPipe

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,10 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
-import { ParseIntPipe } from '@nestjs/common';
+import { ParseIntPipe, ValidationPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto'; 
 import { UpdateUserDto } from './dto/update-user.dto';
 
+@Controller('users')
 export class UsersController {
 
     constructor(private readonly userService: UsersService){}
@@ -21,13 +22,13 @@ export class UsersController {
     }
 
     @Post() // POST /users
-    create(@Body() createUserDto: CreateUserDto)
+    create(@Body(ValidationPipe) createUserDto: CreateUserDto)
     {
         return this.userService.create(createUserDto);
     }
 
     @Patch(':id') // PATCH /users/:id
-    update(@Param('id', ParseIntPipe) id: number, @Body() updateUserDto: UpdateUserDto)
+    update(@Param('id', ParseIntPipe) id: number, @Body(ValidationPipe) updateUserDto: UpdateUserDto)
     {
         return this.userService.update(id, updateUserDto);
     }
