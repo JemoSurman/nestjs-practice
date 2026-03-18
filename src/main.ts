c9ed1acc337a1a6a5a3c00d6453f994ff90a1bfc
JemoSurman <jemosurman@gmail.com>
2026-03-19 00:06:30 +0400

    use Global Filters

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './all-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
 
   app.enableCors();
 
