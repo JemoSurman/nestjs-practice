7bd0a80067efc34225c7a59d88f165b13258423f
JemoSurman <jemosurman@gmail.com>
2026-03-20 00:11:21 +0400

    complete Logger & API

--- src/my-logger/my-logger.service.ts
+++ src/my-logger/my-logger.service.ts
@@ -6,23 +6,25 @@ import * as path from 'path';
 
 @Injectable()
 export class MyLoggerService extends ConsoleLogger {
-    async logToFile(entry){
+    async logToFile(entry) {
+    const logsDir = path.join(process.cwd(), 'logs');
+    const logFile = path.join(logsDir, 'myLogFile.log');
 
     const formattedEntry = `${Intl.DateTimeFormat('en-US', {
         dateStyle: 'short',
         timeStyle: 'short',
         timeZone: 'Asia/Tbilisi',
-    }).format(new Date())}\t${entry}\n`
+    }).format(new Date())}\t${entry}\n`;
 
     try {
-        if (!fs.existsSync(path.join(__dirname, '..', '..', 'logs'))) {
-            await fsPromises.mkdir(path.join(__dirname, '..', '..', 'logs'))
+        if (!fs.existsSync(logsDir)) {
+            await fsPromises.mkdir(logsDir, { recursive: true });
         }
-        await fsPromises.appendFile(path.join(__dirname, '..', '..', 'logs', 'myLogFile.log'), formattedEntry)
-    }catch (e) {
-        if (e instanceof Error) console.error(e.message)
-        }
+        await fsPromises.appendFile(logFile, formattedEntry);
+    } catch (e) {
+        if (e instanceof Error) console.error(e.message);
     }
+}
 
     log(message: any, context?: string){
         const entry = `${context}\t${message}`;
